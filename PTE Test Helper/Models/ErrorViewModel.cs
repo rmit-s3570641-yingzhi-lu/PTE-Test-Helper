4d1e3482006a34e1bf5187fa9a37871c4d26f66b
Jodie Lu <JodieL@panviva.com>
2018-07-21 17:25:20 +1000

    Add project files.

---
+++ PTE Test Helper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PTE_Test_Helper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
